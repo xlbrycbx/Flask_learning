fb647fd5fb8e80f48b8459d4c86e4e6e4bb0e263
xlbrycbx <qinpangji@gmail.com>
2022-05-10 00:43:30 +0800

    V1

---
+++ apps/user/view.py
@@ -0,0 +1,47 @@
+from flask import Blueprint, render_template, request, redirect
+
+from apps.user.model import User
+
+user_bp = Blueprint('user',__name__)
+
+users = []
+
+@user_bp.route('/')
+def user_center():
+    return render_template('user/show.html',users=users)
+
+@user_bp.route('/register', methods=['GET','POST'])
+def user_register():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        repassword = request.form.get('repassword')
+        phone = request.form.get('phone')
+
+        if password == repassword:
+            for user in users:
+                if user.username == username:
+                    return render_template('user/register.html', msg='用户名已存在')
+
+            user = User(username, password, phone)
+            users.append(user)
+
+            return redirect('/')
+
+
+
+
+
+    else:
+        return render_template('user/register.html')
+
+
+@user_bp.route('/login', methods=['GET','POST'])
+def user_login():
+
+    return '用户登录'
+
+@user_bp.route('/logout', methods=['GET','POST'])
+def user_logout():
+
+    return '用户退出'
